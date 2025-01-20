f44c38ab20b86ae2dfc8843b4adb6f5a17052b93
tj <tirtheshjaintj@gmail.com>
2025-01-20 18:05:08 +0530

    Rawar Frontend

--- src/components/ChatBot.tsx
+++ src/components/ChatBot.tsx
@@ -78,7 +78,7 @@ const Chatbot: React.FC = () => {
     <div className="text-gray-800 dark:text-gray-200">
       <button
         onClick={toggleChatbot}
-        className="fixed inline-flex items-center justify-center w-12 h-12 text-white bg-black rounded-full lg:w-14 lg:h-14 bottom-4 right-4 hover:bg-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
+        className="fixed inline-flex z-10 items-center justify-center w-12 h-12 text-white bg-black rounded-full lg:w-14 lg:h-14 bottom-4 right-4 hover:bg-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
       >
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
           <path d="m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z" />
